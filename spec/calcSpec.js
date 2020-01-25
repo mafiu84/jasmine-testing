9e2d24684fe783dc905f41e666e887413b85c4d0
Mateusz Poplewski <matheush84@gmail.com>
2020-01-25 07:04:03 +0000

    additional test added

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -3,5 +3,8 @@ describe("Calculator", function() {
         it("should return 42", function() {
             expect(addition(20, 22)).toBe(42);
         });
+        it("should return 26", function() {
+            expect(addition(7, 19)).toBe(26);
+        });
     });
 });
